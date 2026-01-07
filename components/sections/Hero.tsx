e0ac7fb61914d49d889bfb0d235c668b68bb10b2
fayarsh <faydarsh2000@gmail.com>
2026-01-07 23:07:24 +0530

    fix 3

--- components/sections/Hero.tsx
+++ components/sections/Hero.tsx
@@ -8,7 +8,7 @@ import {
   Play,
 } from "lucide-react";
 
-import { motion } from "framer-motion";
+import { motion,Variants } from "framer-motion";
 export default function HeroSection() {
   const container = {
     hidden: { opacity: 0 },
@@ -20,12 +20,12 @@ export default function HeroSection() {
     },
   };
 
-  const item = {
+  const item: Variants = {
     hidden: { opacity: 0, y: 30 },
     show: {
       opacity: 1,
       y: 0,
-      transition: { duration: 0.6, ease: "easeOut" },
+      transition: { duration: 0.6, ease: [0.25, 0.1, 0.25, 1] },
     },
   };
 
